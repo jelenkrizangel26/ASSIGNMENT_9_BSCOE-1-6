8d72e4733f44d33bdd7fcad3e90858ca0888d363
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-02-10 13:15:18 +0800

    I finally able to put the json file on the pdf but Im having a hard on putting the image. Ill fix that later

--- pdfResumeCreate.py
+++ pdfResumeCreate.py
@@ -16,21 +16,49 @@ from fpdf import FPDF
 import json
 from ctypes import alignment
 
-pdf = FPDF()
-jsonPDF = "pdfResume.json"
-
-#Name and Picture
+#Header
 class PDF(FPDF):
-    def header(me):
-        me.set_font('helvetica', 'B', 28.5)
-        me.cell(0, 30, 'JELEN KRIZ ANGEL P. MAMPUSTI', align = 'R', ln=1)
-        me.ln(3)
+    def header(self):
+        self.image('RESUME_PIC', 10, 8, 40)
+        self.ln(3)
 
-    def image(self):
-        self.image('RESUME_PIC.jpg', 10, 8, 40)
+#Format
+pdf = FPDF('P','mm', 'Legal')
+pdf.set_auto_page_break(auto=1, margin=55.4)
+pdf.add_page()
+
+#Short
+jsonPDF = open('pdfResume.json', 'r')
+Create = jsonPDF.read()
+Info = json.loads(Create)
+
 
 # Personal Information
-for information in Base_info:
+for information in Info:
+    pdf.set_font('helvetica', "B", 12)
+    pdf.cell(0,5, f"{information['Name']}", align='L', ln=1)
+    pdf.set_font('helvetica', "", 12)
+    pdf.cell(0,5, f"{information['Address']}", align='L', ln=1)
+    pdf.cell(0,5, f"{information['E-mail']}", align='L', ln=1)
+    pdf.cell(0,5, f"{information['Contact No.']}", align='L', ln=1)
+    pdf.ln(12)
+
+    pdf.set_font('helvetica', "B", 15)
+    pdf.cell(0, 10, f"{information['head1']}", 'BI', ln=1)
+    pdf.ln(3)
+    pdf.set_font('helvetica', "I", 12)
+    pdf.multi_cell(0, 5, f"{information['Job_Obj']}", 'BI', align='L', ln=1)
+    pdf.ln(10)
+
+    pdf.set_font('helvetica', "B", 15)
+    pdf.cell(0, 10, f"{information['head2']}", ln=1)
+    pdf.ln(5)
+    pdf.set_font('helvetica', "", 12)
+
+
+
+
+
     
 
 
